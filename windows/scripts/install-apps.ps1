5fbee54c0b588e153de2f284327d6bf6ef0915e3
Simon Malm <simon.v.p.malm@gmail.com>
2023-06-25 22:31:49 +0200

    windows scripts wip

---
+++ windows/scripts/install-apps.ps1
@@ -0,0 +1,13 @@
+# ---------------------------------------------- #
+# Browsers  ------------------------------------ #
+# ---------------------------------------------- #
+choco install -y googlechrome
+choco install -y firefox
+
+# ---------------------------------------------- #
+# Common tools  --------------------------------- #
+# ---------------------------------------------- #
+choco install -y 7zip
+choco install -y make
+choco install -y neovim --pre
+choco install -y ripgrep fd golang SQLite universal-ctags 
