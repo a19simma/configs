d8e1bd48a3143d68a6bbe3dd03e0a07a916a1af1
Simon Malm <simon.v.p.malm@gmail.com>
2023-06-25 19:37:07 +0200

    wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,17 +7,51 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+    config = wezterm.config_builder()
 end
 
--- config.window_decorations = "NONE"
+config.window_decorations = "RESIZE"
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.font = wezterm.font_with_fallback({ 'JetBrains Mono', 'Hack', 'Symbols Nerd Font', 'Noto Sans CJK TC' })
+config.font = wezterm.font_with_fallback({'JetBrains Mono', 'Hack', 'Symbols Nerd Font', 'Noto Sans CJK TC'})
 config.color_scheme = 'Rosé Pine (base16)'
-config.default_prog = { 'C:\\Program Files\\PowerShell\\7\\pwsh.exe' }
+config.default_prog = {'C:\\Program Files\\PowerShell\\7\\pwsh.exe'}
 config.window_background_opacity = 0.85
+config.adjust_window_size_when_changing_font_size = false
+
+config.enable_tab_bar = false
+
+config.keys = {{
+    key = 'w',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.CloseCurrentPane {
+        confirm = true
+    }
+}}
+
+config.wsl_domains = {{
+    name = 'wsl',
+    distribution = 'Debian'
+}}
+
+config.ssh_domains = {{
+    name = 'wslix',
+    remote_address = 'INEX-FPFMKN3.int.inexchange.com:2222',
+    username = 'root',
+    ssh_option = {
+        identityfile = 'C:\\Users\\simon\\main-key',
+    },},
+}
+
+config.ssh_domains = {{
+    name = 'ix',
+    remote_address = 'INEX-FPFMKN3.int.inexchange.com:6922',
+    username = 'simon.malm',
+    ssh_option = {
+        identityfile = 'C:\\Users\\simon\\main-key',
+    },},
+}
 
 -- and finally, return the configuration to wezterm
 return config
