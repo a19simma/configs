5cf821bf803698fd4293a992986678486d830ddc
Simon Malm <simon.v.p.malm@gmail.com>
2023-06-26 16:53:23 +0200

    glaze-wm config

--- windows/scripts/apply-configs.ps1
+++ windows/scripts/apply-configs.ps1
@@ -6,7 +6,7 @@ Remove-Item -Path "$env:USERPROFILE\.wezterm.lua" -Force > $null
 New-Item -ItemType SymbolicLink  -Path "$env:USERPROFILE\.wezterm.lua" -Target "$env:USERPROFILE\Documents\configs\wezterm\.wezterm.lua" -Force > $null
 
 Remove-Item -Path "$env:USERPROFILE\.glaze-wm\config.yaml" -Force > $null
-New-Item -ItemType SymbolicLink  -Path "$env:USERPROFILE\.glaze-wm\config.yaml" -Target "$env:USERPROFILE\.glaze-wm\config.yaml" -Force > $null
+New-Item -ItemType SymbolicLink  -Path "$env:USERPROFILE\.glaze-wm\config.yaml" -Target "$env:USERPROFILE\Documents\glaze-wm\config.yaml" -Force > $null
 
 Remove-Item -Path "$HOME\Appdata\Local\nvim" -Recurse  -Force > $null
 New-Item -ItemType Junction  -Path "$HOME\Appdata\Local\nvim" -Target "$env:USERPROFILE\Documents\configs\nvim" -Force > $null
