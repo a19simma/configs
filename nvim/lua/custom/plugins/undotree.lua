c2cd615f8c89bd6d1ded918f393130f852e0889f
Simon Malm <simon.v.p.malm@gmail.com>
2023-10-29 19:59:44 +0100

    feat: unimpaired, repeat and refactor

--- nvim/lua/custom/plugins/undotree.lua
+++ nvim/lua/custom/plugins/undotree.lua
@@ -2,5 +2,7 @@ return {
   'mbbill/undotree',
   config = function()
     vim.keymap.set('n', '<leader>ut', vim.cmd.UndotreeToggle)
+    vim.g.undotree_SetFocusWhenToggle = 1
+    vim.g.undotree_WindowLayout = 3
   end
 }
