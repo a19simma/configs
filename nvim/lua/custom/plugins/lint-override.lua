26725ac547cd0dfd7e5849f198694fe3c0b896d5
Simon Malm <simon.v.p.malm@gmail.com>
2025-08-11 09:55:03 +0200

    feat: add fugitive and undotree
    This commit adds more plugins and configurations

---
+++ nvim/lua/custom/plugins/lint-override.lua
@@ -0,0 +1,10 @@
+-- Override lint configuration to disable markdown linting
+return {
+	"mfussenegger/nvim-lint",
+	opts = function()
+		local lint = require("lint")
+		-- Remove markdown linting to avoid permission errors
+		lint.linters_by_ft = lint.linters_by_ft or {}
+		lint.linters_by_ft.markdown = nil
+	end,
+}
---
+++ nvim/lua/custom/plugins/neotree.lua
@@ -0,0 +1,6 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	keys = {
+		{ "<leader>ft", "<cmd>Neotree toggle<cr>", desc = "Toggle file tree" },
+	},
+}
